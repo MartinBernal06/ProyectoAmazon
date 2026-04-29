620c630e5781a81b676414be401e0ce5725e0cc6
DavidTL001 <david.tirado233047@potros.itson.edu.mx>
2026-04-29 17:53:39 -0600

    Create Direccion.java

---
+++ src/main/java/models/Direccion.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author David TL
+ */
+public class Direccion {
+    private String calle;
+    private String numero;
+    private String cPostal;
+    private String colonia;
+
+    public Direccion() {
+    }
+
+    public Direccion(String calle, String numero, String cPostal, String colonia) {
+        this.calle = calle;
+        this.numero = numero;
+        this.cPostal = cPostal;
+        this.colonia = colonia;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getcPostal() {
+        return cPostal;
+    }
+
+    public void setcPostal(String cPostal) {
+        this.cPostal = cPostal;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+    
+}
