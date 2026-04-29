6f019871e883e168579feaa707d9b92bf0e7bf2e
DavidTL001 <david.tirado233047@potros.itson.edu.mx>
2026-04-29 17:53:34 -0600

    Create Proveedor.java

---
+++ src/main/java/models/Proveedor.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import org.bson.types.ObjectId;
+
+/**
+ *
+ * @author David TL
+ */
+public class Proveedor {
+    private String nombre;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
